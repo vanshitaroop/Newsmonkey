6428dc0933c2f102a4bee6a63dd4800fb6a783e1
vanshitaroop <vanshitaroopchandani7@gmail.com>
2022-11-30 01:03:42 +0530

    refactoring of code

--- src/components/News.js
+++ src/components/News.js
@@ -16,14 +16,31 @@ export default class News extends Component {
     pageSize : PropTypes.number,
     category: PropTypes.string,
   }
-  constructor(){
-    super()
+  capitalizeFirstLetter=(string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  constructor(props){
+    super(props)
     console.log("tis is a constructor");
     this.state={
-      page:1,
+      page:2,
       articles:[],
       loading:false
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} -NewsMonkey`
+  }
+  async updateNews(){
+    const url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d201a4d55c44269973a3cdee179d3d0&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    console.log(this.state.page)
+    this.setState({loading:true})
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData); 
+    this.setState({
+      totalResults:parsedData.totalResults,
+      articles:parsedData.articles,
+      loading:false
+    })
   }
   async componentDidMount(){
     console.log("I am cmd");
@@ -38,46 +55,51 @@ export default class News extends Component {
 
   }
   handleNextClick= async()=>{
-    console.log("next");
-   if (!(this.state.page+1>Math.ceil(this.state.totalResults/this.state.pageSize))){
-    let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d201a4d55c44269973a3cdee179d3d0&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData); 
-    this.setState({
-      page:this.state.page+1,
-      articles:parsedData.articles,
-      loading:false
-    })
+    
+  //  if (!(this.state.page+1>Math.ceil(this.state.totalResults/this.state.pageSize))){
+  //   let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d201a4d55c44269973a3cdee179d3d0&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+  //   this.setState({loading:true})
+  //   let data = await fetch(url);
+  //   let parsedData = await data.json()
+  //   console.log(parsedData); 
+  //   this.setState({
+  //     page:this.state.page+1,
+  //     articles:parsedData.articles,
+  //     loading:false
+  //   })
+  this.setState({page:this.state.page + 1})
+  this.updateNews()
   }
   
    
-  }
+  
   handlePrevClick= async ()=>{
-    console.log("prev");
-    let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d201a4d55c44269973a3cdee179d3d0&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData); 
-    this.setState({
-      page:this.state.page-1,
-      articles:parsedData.articles,
-      loading:false
-    })
+    // console.log("prev");
+    // let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8d201a4d55c44269973a3cdee179d3d0&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading:true})
+    // let data = await fetch(url);
+    // let parsedData = await data.json()
+    // console.log(parsedData); 
+    // this.setState({
+    //   page:this.state.page-1,
+    //   articles:parsedData.articles,
+    //   loading:false
+    // })
+    this.setState({page:this.state.page - 1})
+    this.updateNews()
+    
   }
   render() {
     console.log("render")
     return (
      <>
        <div className='container my-4'>
-        <h1 className='text-center'>NewsMonkey -Top Headlines</h1>
+        <h1 className='text-center my-5'>NewsMonkey -Top Headlines from {this.capitalizeFirstLetter(this.props.category)}</h1>
         {this.state.loading && <Spinner/>}
         <div className='row'>
         {!this.state.loading && this.state.articles.map((element)=>{
             return <div className='col-md-4' key={element.url}>
-            <Newsitem title={element.title?element.title.slice(0,44):" "} description={element.description?element.description.slice(0,88):" "} newsUrl={element.url} imageUrl={element.urlToImage}/>
+            <Newsitem title={element.title?element.title.slice(0,44):" "} description={element.description?element.description.slice(0,88):" "} newsUrl={element.url} imageUrl={element.urlToImage} author={element.author?element.author:"Unknown"} date={element.publishedAt} source={element.source.name}/>
             </div>
         })}
         </div>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Newsitem extends Component {
-  
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, date, author,source } = this.props;
     return (
-      <div className='my-3'>
-           <div className="card" style={{ backgroundColor:"#F9FCFF"}}>
-              <img src={imageUrl?imageUrl:"http://afiedu.com/wp-content/uploads/2019/03/New-4.jpg"} className="card-img-top" alt="..."/>
-              <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
-              <a href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
-            </div>
-    </div>
+      <div className="my-3">
+        <div className="card" style={{ backgroundColor: "#F9FCFF" }}>
+        <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:"90%" , zIndex:"1"}}>
+              {source}
+            </span>
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "http://afiedu.com/wp-content/uploads/2019/03/New-4.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+           
+            <p class="card-text">
+              <small class="text-muted">
+                publish by {author} on {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a href={newsUrl} className="btn btn-sm btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
